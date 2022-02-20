3a7af8b8b6359da2823349464ebc43e936e92611
Satyam <satyam@satyam.com.ar>
2022-02-20 17:59:40 +0100

    Changed lit build to bundling by rollup. Since the `public` folder is shared by all builds, I added a step to clear it before each compulation.

--- lit-client/rollup.config.js
+++ lit-client/rollup.config.js
@@ -1,13 +1,13 @@
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
 import clear from 'rollup-plugin-clear';
-import bundleHtml from './buildhtml';
 import { join } from 'path';
 
 const PUBLIC = '../public';
 const node_modules = '../node_modules';
 export default {
-  input: 'src/index.ts',
+  input: 'src/my-element.ts',
   output: {
     dir: PUBLIC,
     format: 'es',
@@ -17,12 +17,19 @@ export default {
     clear({
       targets: [PUBLIC],
     }),
+    resolve(),
     typescript(),
     copy({
       targets: [
         {
           src: [
+            'index.html',
             'src/assets/*',
+            join(
+              node_modules,
+              '@webcomponents/webcomponentsjs/webcomponents-loader.js'
+            ),
+            join(node_modules, 'lit/polyfill-support.js'),
             join(node_modules, 'bootstrap/dist/css/bootstrap.min.css'),
             join(node_modules, 'bootstrap-icons/font/bootstrap-icons.css'),
           ],
@@ -37,6 +44,5 @@ export default {
         },
       ],
     }),
-    bundleHtml({ dest: join(PUBLIC, 'index.html') }),
   ],
 };
