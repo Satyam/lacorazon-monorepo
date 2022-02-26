bcf7c59d3ea493dd3d34bf1e37240b4d00d9b22d
Satyam <satyam@satyam.com.ar>
2022-02-26 16:43:23 +0100

    Fixed: added `show=true` to route for `<not-found>`

--- lit-client/src/index.ts
+++ lit-client/src/index.ts
@@ -66,7 +66,7 @@ const routes: Array<Route> = [
   // },
   {
     path: '*',
-    litElement: () => html`<not-found></not-found>`,
+    litElement: () => html`<not-found ?show=${true}></not-found>`,
   },
 ];
 
