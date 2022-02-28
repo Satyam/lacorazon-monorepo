5b787c57b75bbf4f1ed7e7bd0719f3cd7d50342e
Satyam <satyam@satyam.com.ar>
2022-02-28 19:14:11 +0100

    Added date and currency formatter to utils

--- lit-client/src/utils.ts
+++ lit-client/src/utils.ts
@@ -6,6 +6,28 @@ export const getClosest = <T extends HTMLElement>(
 export const getTarget = <T extends HTMLElement>(ev: Event): T =>
   ev.target as T;
 
+const currency = 'EUR';
+const locale = 'es-ES';
+
+const dateFormatter = new Intl.DateTimeFormat(locale, {
+  dateStyle: 'medium',
+});
+
+export const formatDate = (date: Date | string) =>
+  date instanceof Date
+    ? dateFormatter.format(date)
+    : date
+    ? dateFormatter.format(new Date(date))
+    : '';
+
+const currFormatter = new Intl.NumberFormat(locale, {
+  style: 'currency',
+  currency,
+});
+
+export const formatCurrency = (value?: number) =>
+  typeof value === 'undefined' ? '' : currFormatter.format(value);
+
 export const ROUTER_EVENT: 'router' = 'router' as const;
 
 type RouterEventDetail = {
