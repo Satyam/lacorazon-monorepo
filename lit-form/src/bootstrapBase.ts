693353bb4d08e6f360517e001aa4a0b119df9dfa
Satyam <satyam@satyam.com.ar>
2022-03-26 15:09:09 +0100

    In `lit-form` make sure `BootBase` picks all the stylesheets loaded

--- lit-form/src/bootstrapBase.ts
+++ lit-form/src/bootstrapBase.ts
@@ -3,8 +3,12 @@ import { LitElement, unsafeCSS } from 'lit';
 export class BootBase extends LitElement {
   static override readonly styles = [
     unsafeCSS(
-      Array.from(document.styleSheets[0].cssRules)
-        .map((r) => r.cssText)
+      Array.from(document.styleSheets)
+        .map((ss) =>
+          Array.from(ss.cssRules)
+            .map((r) => r.cssText)
+            .join('\n')
+        )
         .join('\n')
     ),
   ];
