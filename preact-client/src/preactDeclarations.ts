fb7d99016314b7f8cce82bd92b26c755b5b39eef
Satyam <satyam@satyam.com.ar>
2022-03-27 18:10:23 +0200

    Added the preact declaration for form events, the way it wants it, with an `on` prefix and no case change

--- preact-client/src/preactDeclarations.ts
+++ preact-client/src/preactDeclarations.ts
@@ -1,27 +1,26 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 // import { ComponentChildren, createElement } from 'preact';
-import { FormSubmit } from '@lacorazon/lit-form';
+import { FormSubmit, FormChanged } from '@lacorazon/lit-form';
 import 'preact';
 declare module 'preact/src/jsx' {
   namespace JSXInternal {
-    // We're extending the IntrinsicElements interface which holds a kv-list of
-    // available html-tags.
     interface IntrinsicElements {
       'form-wrapper': React.HTMLAttributes<HTMLFormElement> & {
-        onFormSubmit?: (ev: FormSubmit) => void;
+        onformSubmit?: (ev: FormSubmit) => void;
+        onformChanged?: (ev: FormChanged) => void;
       };
       'text-field': React.HTMLAttributes<HTMLInputElement> & {
         label: string;
         name: string;
         value?: string;
         readonly?: boolean;
-      }; // Web component extended from input
+      };
       'email-field': React.HTMLAttributes<HTMLInputElement> & {
         label: string;
         name: string;
         value?: string;
         readonly?: boolean;
-      }; // Web component extended from input
+      };
     }
   }
 }
