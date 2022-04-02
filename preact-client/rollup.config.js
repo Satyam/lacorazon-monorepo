3cc22ac89760d87d758a4997362de1058bf78b7e
Satyam <satyam@satyam.com.ar>
2022-04-02 19:31:26 +0200

    Dropped `terser` plugin from preact-client build

--- preact-client/rollup.config.js
+++ preact-client/rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 import clear from 'rollup-plugin-clear';
 import copy from 'rollup-plugin-copy';
 import postcss from 'rollup-plugin-postcss';
@@ -48,7 +47,6 @@ export default {
     postcss({
       modules: true,
     }),
-    terser(),
     copy({
       targets: [
         {
