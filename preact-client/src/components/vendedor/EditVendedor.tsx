9f564693ab2c9ac2795de71d2d16eaed5fb7caa4
Satyam <satyam@satyam.com.ar>
2022-04-03 14:39:38 +0200

    EditVendedor:  Added handling of errors for delete, create and update ops

--- preact-client/src/components/vendedor/EditVendedor.tsx
+++ preact-client/src/components/vendedor/EditVendedor.tsx
@@ -1,5 +1,6 @@
 import { h } from 'preact';
 import { route } from 'preact-router';
+import { useState } from 'preact/hooks';
 import { Alert } from 'react-bootstrap';
 
 import Page from 'components/Page';
@@ -23,71 +24,59 @@ export const EditVendedor = ({ id }: { id: ID }) => {
   const {
     isLoading,
     isError,
-    error,
+    error: fetchError,
     data: vendedor,
   } = useQuery<Vendedor, Error>(
     [VENDEDORES_KEY, id],
     () => apiGetVendedor(id),
     { enabled: !!id }
   );
+
+  const [error, setError] = useState<Error | null>(null);
+
   const queryClient = useQueryClient();
 
   const deleteVendedor = useMutation<null, Error, ID>(apiRemoveVendedor, {
-    onMutate: () => {
-      openLoading('Borrando vendedor');
-    },
+    onMutate: () => openLoading('Borrando vendedor'),
     onSuccess: () => {
       // Invalidate and refetch
       queryClient.invalidateQueries(VENDEDORES_KEY);
       route('/vendedores', true);
     },
-    onError: (error, id) => {
-      console.error('on:', id, 'error:', error.message);
-    },
-    onSettled: () => {
-      closeLoading();
-    },
+    onError: (error) => setError(error),
+    onSettled: () => closeLoading(),
   });
 
   const createVendedor = useMutation<Vendedor, Error, Vendedor>(
     apiCreateVendedor,
     {
-      onMutate: () => {
-        openLoading('Creando vendedor');
-      },
+      onMutate: () => openLoading('Creando vendedor'),
       onSuccess: ({ id }) => {
         // Invalidate and refetch
         queryClient.invalidateQueries(VENDEDORES_KEY);
         route(`/vendedor/edit/${id}`, true);
       },
-      onError: (error, vendedor) => {
-        console.error('Error on create:', error.message, vendedor);
-      },
-      onSettled: () => {
-        closeLoading();
-      },
+      onError: (error) => setError(error),
+      onSettled: () => closeLoading(),
     }
   );
   const updateVendedor = useMutation<Vendedor, Error, Vendedor>(
     apiUpdateVendedor,
     {
-      onMutate: () => {
-        openLoading('Actualizando usuario');
-      },
+      onMutate: () => openLoading('Actualizando usuario'),
       onSuccess: () => {
         queryClient.invalidateQueries([VENDEDORES_KEY, id]);
       },
-      onError: (error, vendedor) => {
-        console.error('Error on update:', error.message, vendedor);
-      },
-      onSettled: () => {
-        closeLoading();
-      },
+      onError: (error) => setError(error),
+      onSettled: () => closeLoading(),
     }
   );
   const { openLoading, closeLoading, confirmDelete } = useModals();
 
-  if (isError) return <Alert variant="warning">{error.message}</Alert>;
+  // All hooks executed, now I can branch
+  if (isError) setError(fetchError);
+
+  if (error) return <Alert variant="warning">{error.toString()}</Alert>;
   if (isLoading) return <Loading>Cargando vendedor</Loading>;
 
   const onDeleteClick = () => {
