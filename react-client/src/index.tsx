fae9910ed28176b0a1b479fafcbd449dde6ebd61
Satyam <satyam@satyam.com.ar>
2022-07-09 17:22:23 +0200

    Moved the `reactDeclarations` file to the shared `types` folder

--- react-client/src/index.tsx
+++ react-client/src/index.tsx
@@ -1,8 +1,8 @@
 /// <reference path="../../types/global.d.ts" />
 /// <reference path="../../types/api.d.ts" />
+/// <reference path="../../types/reactIconDeclarations.d.ts" />
 import { createRoot } from 'react-dom/client';
 import Routes from 'components/Routes';
-import './reactDeclarations';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(<Routes />);
