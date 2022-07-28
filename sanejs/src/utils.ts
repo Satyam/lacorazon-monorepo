a85a605f4806803b22ed5f588514e0fe7e5a2f48
Satyam <satyam@satyam.com.ar>
2022-07-28 18:25:05 +0200

    Moved formatDate and formatCurrency to separate utils.js file

---
+++ sanejs/src/utils.ts
@@ -0,0 +1,21 @@
+const currency = 'EUR';
+const locale = 'es-ES';
+
+const dateFormatter = new Intl.DateTimeFormat(locale, {
+  dateStyle: 'medium',
+});
+
+export const formatDate = (date: Date | string) =>
+  date instanceof Date
+    ? dateFormatter.format(date)
+    : date
+    ? dateFormatter.format(new Date(date))
+    : '';
+
+const currFormatter = new Intl.NumberFormat(locale, {
+  style: 'currency',
+  currency,
+});
+
+export const formatCurrency = (value?: number) =>
+  typeof value === 'undefined' ? '' : currFormatter.format(value);
