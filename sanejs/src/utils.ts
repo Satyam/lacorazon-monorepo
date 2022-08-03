eec11f7752ac079be7fb8ef76a605e961c9eb4c1
Satyam <satyam@satyam.com.ar>
2022-08-03 19:00:50 +0200

    Added `datePart` function to `utils.ts` to format date for `<input type="date">` elements

--- sanejs/src/utils.ts
+++ sanejs/src/utils.ts
@@ -21,3 +21,12 @@ export const formatCurrency = (value?: number) =>
   typeof value === 'undefined' ? '' : currFormatter.format(value);
 
 export const allSections = ['main', 'heading', 'title'];
+
+export const datePart = (ds: Date | string = new Date()) => {
+  const d = ds instanceof Date ? ds : new Date(ds);
+  return [
+    d.getFullYear().toString().padStart(4, '0'),
+    (d.getMonth() + 1).toString().padStart(2, '0'),
+    d.getDate().toString().padStart(2, '0'),
+  ].join('-');
+};
