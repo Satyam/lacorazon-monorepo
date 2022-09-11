876801f41b9f5b161c54c79ca13d88a93fcfc1f4
Satyam <satyam@satyam.com.ar>
2022-09-11 16:08:27 +0200

    CHanged the generic `fila` to a more specific `venta`

--- rest-server/src/ventas.ts
+++ rest-server/src/ventas.ts
@@ -11,9 +11,9 @@ import {
 export const listVentas = (db: Database) => listAll<Venta[]>(db, TABLE_VENTAS);
 export const getVenta = (db: Database, id: ID) =>
   getById<Venta>(db, TABLE_VENTAS, id);
-export const createVenta = (db: Database, fila: Venta) =>
-  createWithCuid<Venta>(db, TABLE_VENTAS, fila);
-export const updateVenta = (db: Database, id: ID, fila: Venta) =>
-  updateById<Venta>(db, TABLE_VENTAS, id, fila);
+export const createVenta = (db: Database, venta: Venta) =>
+  createWithCuid<Venta>(db, TABLE_VENTAS, venta);
+export const updateVenta = (db: Database, id: ID, venta: Venta) =>
+  updateById<Venta>(db, TABLE_VENTAS, id, venta);
 export const deleteVenta = (db: Database, id: ID) =>
   deleteById(db, TABLE_VENTAS, id);
