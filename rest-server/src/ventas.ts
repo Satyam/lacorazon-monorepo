a905a30f22f21621a89d64a0c0ab7942c048ab5e
Satyam <satyam@satyam.com.ar>
2022-09-16 15:47:31 +0200

    In `ventas` of `rest-server` make the join to get the  `nombre` of the vendedor.

--- rest-server/src/ventas.ts
+++ rest-server/src/ventas.ts
@@ -1,16 +1,33 @@
 import { Database } from 'sqlite';
 import {
-  getById,
-  listAll,
   createWithCuid,
   updateById,
   deleteById,
   TABLE_VENTAS,
+  TABLE_VENDEDORES,
 } from './utils.js';
 
-export const listVentas = (db: Database) => listAll<Venta[]>(db, TABLE_VENTAS);
+export const listVentas = (db: Database, idVendedor?: ID) => {
+  idVendedor
+    ? db.all(
+        `select * from ${TABLE_VENTAS} where idVendedor = $idVendedor order by fecha, id`,
+        {
+          $idVendedor: idVendedor,
+        }
+      )
+    : db.all(
+        `select ${TABLE_VENTAS}.*, ${TABLE_VENDEDORES}.nombre as vendedor from ${TABLE_VENTAS}
+              left join ${TABLE_VENDEDORES} on ${TABLE_VENTAS}.idVendedor = ${TABLE_VENDEDORES}.id  
+              order by fecha, id`
+      );
+};
 export const getVenta = (db: Database, id: ID) =>
-  getById<Venta>(db, TABLE_VENTAS, id);
+  db.get(
+    `select ${TABLE_VENTAS}.*, ${TABLE_VENDEDORES}.nombre as vendedor from ${TABLE_VENTAS}
+      left join ${TABLE_VENDEDORES} on ${TABLE_VENTAS}.idVendedor = ${TABLE_VENDEDORES}.id  
+      where ${TABLE_VENTAS}.id = $id`,
+    { $id: id }
+  );
 export const createVenta = (db: Database, venta: Venta) =>
   createWithCuid<Venta>(db, TABLE_VENTAS, venta);
 export const updateVenta = (db: Database, id: ID, venta: Venta) =>
