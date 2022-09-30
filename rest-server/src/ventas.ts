a098802a0efde311db8eb9f9c7aa42072490bb7e
Satyam <satyam@satyam.com.ar>
2022-09-30 19:46:00 +0200

    ventas should be created with AutoId, not CUID

--- rest-server/src/ventas.ts
+++ rest-server/src/ventas.ts
@@ -1,10 +1,10 @@
 import { Database } from 'sqlite';
 import {
-  createWithCuid,
   updateById,
   deleteById,
   TABLE_VENTAS,
   TABLE_VENDEDORES,
+  createWithAutoId,
 } from './utils.js';
 
 export const listVentas = (db: Database, idVendedor?: ID) =>
@@ -30,7 +30,7 @@ export const getVenta = (db: Database, id: ID) =>
     { $id: id }
   );
 export const createVenta = (db: Database, venta: Venta) =>
-  createWithCuid<Venta>(db, TABLE_VENTAS, venta);
+  createWithAutoId<Venta>(db, TABLE_VENTAS, venta);
 export const updateVenta = (db: Database, id: ID, venta: Venta) =>
   updateById<Venta>(db, TABLE_VENTAS, id, venta);
 export const deleteVenta = (db: Database, id: ID) =>
