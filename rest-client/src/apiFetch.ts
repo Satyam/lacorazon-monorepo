c87b4079789a6428620719e95b322d5af6eacc67
Satyam <satyam@satyam.com.ar>
2022-10-10 18:12:23 +0200

    Fixed `apiFetch.ts` in `rest-client`. - make all the upper-level `apXxxx` functions call `apiFetch`. - In `apiFetch` use the same logic to decode the response and the errors as in the testUtils. - In the `apiXxx` functions that pass a body, ensure it is JSON encoded and the header sent. - in `apiList` send the options as query string, not in the body.

--- rest-client/src/apiFetch.ts
+++ rest-client/src/apiFetch.ts
@@ -1,46 +1,57 @@
-export const apiFetch = <Out>(serviceUrl: string, options?: RequestInit) =>
-  fetch(`/api/${serviceUrl}`, options)
-    .then((response) => response.json())
-    .then((data) => data as Out);
+export const apiFetch = async <Out>(
+  serviceUrl: string,
+  options?: RequestInit
+) => {
+  const resp = await fetch(`/api/${serviceUrl}`, options);
+  const body = await resp.text();
+  if (resp.ok) {
+    return JSON.parse(body) as Out;
+  }
+  const err = new Error(resp.statusText);
+  // @ts-ignore
+  err.code = resp.status;
+  // @ts-ignore
+  err.msg = resp.statusText.replace('Error:', '').trim();
+  throw err;
+};
 
-export const apiList = <Out>(service: string, idVendedor?: ID): Promise<Out> =>
-  fetch(`/api/${service}`, {
-    body: { idVendedor } as unknown as BodyInit,
-  })
-    .then((response) => response.json())
-    .then((data) => data as Out);
+export const apiList = <Out>(
+  service: string,
+  options?: Record<string, string>
+): Promise<Out> =>
+  apiFetch(
+    options
+      ? `${service}?${new URLSearchParams(Object.entries(options))}`
+      : service
+  );
 
 export const apiGet = <Out>(service: string, id: ID): Promise<Out> =>
-  fetch(`/api/${service}/${id}`)
-    .then((response) => response.json())
-    .then((data) => data as Out);
+  apiFetch(`${service}/${id}`);
 
 export const apiCreate = <Out>(
   service: string,
   row: Partial<Out>
 ): Promise<Out> =>
-  fetch(`/api/${service}`, {
+  apiFetch(service, {
     method: 'POST',
-    body: row as unknown as BodyInit,
-  })
-    .then((response) => response.json())
-    .then((data) => data as Out);
+    body: JSON.stringify(row),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
 export const apiUpdate = <Out>(
   service: string,
   id: ID,
   row: Partial<Out>
 ): Promise<Out> =>
-  fetch(`/api/${service}/${id}`, {
+  apiFetch(`${service}/${id}`, {
     method: 'PUT',
-    body: row as unknown as BodyInit,
-  })
-    .then((response) => response.json())
-    .then((data) => data as Out);
+    body: JSON.stringify(row),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
 export const apiDelete = (service: string, id: ID): Promise<undefined> =>
-  fetch(`/api/${service}/${id}`, {
+  apiFetch(`${service}/${id}`, {
     method: 'DELETE',
-  }).then(() => undefined);
+  });
 
 export default apiFetch;
