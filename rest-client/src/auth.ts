520fc041c7365c467992ce1affa067b594b40625
Satyam <satyam@satyam.com.ar>
2022-10-11 11:02:52 +0200

    Adapt `auth.ts` in `rest-client` to updated api on `rest-server`

--- rest-client/src/auth.ts
+++ rest-client/src/auth.ts
@@ -7,10 +7,14 @@ export type LoginInfo = Pick<User, 'email' | 'password'>;
 export const AUTH_SERVICE = 'auth';
 
 export const apiLogin = (loginInfo: LoginInfo) =>
-  apiFetch<User>(`/api/${AUTH_SERVICE}/login`, { body: loginInfo as BodyInit });
+  apiFetch<User>(`${AUTH_SERVICE}/login`, {
+    method: 'POST',
+    body: JSON.stringify(loginInfo),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
 export const apiLogout = () =>
-  apiFetch<undefined>(`/api/${AUTH_SERVICE}/logout`);
+  apiFetch<undefined>(`${AUTH_SERVICE}/logout`, { method: 'POST' });
 
-export const apiIsLoggedIn = () =>
-  apiFetch<boolean>(`/api/${AUTH_SERVICE}/isLoggedInlogin`);
+export const apiCurrentUser = () =>
+  apiFetch<boolean>(`${AUTH_SERVICE}/currentUser`);
