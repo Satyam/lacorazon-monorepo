722ce583e92398122e51c2485ec8e7e6de91a6a7
Satyam <satyam@satyam.com.ar>
2025-08-18 18:28:26 +0200

    Missed deleting a TypeScript file

--- juris-client/index.juris.js
+++
@@ -1,7 +0,0 @@
-/**
- * @typedef {import('juris').JurisElementOptions} JurisElementOptions
- * @typedef {import('juris').JurisVDOMElement} JurisVDOMElement
- * @typedef {import('juris').JurisContextBase} JurisContextBase
- * @typedef {import('juris').JurisContext} JurisContext
- * @typedef {import('juris').JurisLifecycleComponent} JurisLifecycleComponent
- */
