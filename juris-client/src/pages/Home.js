0afe2eb46070680d92a42fab532d02127aa4780d
Satyam <satyam@satyam.com.ar>
2025-08-22 18:59:01 +0200

    Dropped some debugging data from the Home page

--- juris-client/src/pages/Home.js
+++ juris-client/src/pages/Home.js
@@ -1,19 +1,13 @@
-juris.registerComponent(
-  'Home',
-  (props, { setState, getState, services, utils }) => ({
-    render: () => {
-      setState('title', 'Bienvenido');
-      return {
-        pre: {
-          text:
-            'state:\n' +
-            JSON.stringify(juris.stateManager.state, null, 2) +
-            '\nservices\n' +
-            JSON.stringify(services, null, 2) +
-            '\nheadless\n' +
-            JSON.stringify(utils.getHeadlessStatus(), null, 2),
-        },
-      };
-    },
-  })
-);
+juris.registerComponent('Home', (props, { setState, getState }) => ({
+  render: () => {
+    setState('title', 'Bienvenido');
+    return {
+      pre: {
+        text: () =>
+          JSON.stringify(getState('data'), null, 1) +
+          '\nstate:\n' +
+          JSON.stringify(juris.stateManager.state, null, 2),
+      },
+    };
+  },
+}));
