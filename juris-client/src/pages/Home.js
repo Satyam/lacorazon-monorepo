12f5ba3a3323f397320b104f49284028307f8136
Satyam <satyam@satyam.com.ar>
2025-08-30 18:50:14 +0200

    Sick of seeing that  initial state display, in the home page,  which doesn't update when the state changes.

--- juris-client/src/pages/Home.js
+++ juris-client/src/pages/Home.js
@@ -1,11 +1,11 @@
 juris.registerComponent('Home', (props, { setState, getState }) => ({
   render: () => {
     setState('title', 'Bienvenido');
-    return {
-      pre: {
-        text: () =>
-          '\nstate:\n' + JSON.stringify(juris.stateManager.state, null, 2),
-      },
-    };
+    // return {
+    //   pre: {
+    //     text: () =>
+    //       '\nstate:\n' + JSON.stringify(juris.stateManager.state, null, 2),
+    //   },
+    // };
   },
 }));
