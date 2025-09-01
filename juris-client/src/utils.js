774bd481316f788629f21ba2fbaa48e0f8e11d39
Satyam <satyam@satyam.com.ar>
2025-09-01 18:25:16 +0200

    Added a `util.js` with various bits and pieces

---
+++ juris-client/src/utils.js
@@ -0,0 +1,27 @@
+const currency = 'EUR';
+const locale = 'es-ES';
+
+const dateFormatter = new Intl.DateTimeFormat(locale, {
+  dateStyle: 'medium',
+});
+
+export const formatDate = (date) =>
+  date instanceof Date
+    ? dateFormatter.format(date)
+    : date
+    ? dateFormatter.format(new Date(date))
+    : '';
+
+const currFormatter = new Intl.NumberFormat(locale, {
+  style: 'currency',
+  currency,
+});
+
+export const formatCurrency = (value) =>
+  typeof value === 'undefined' ? '' : currFormatter.format(value);
+
+export const iconCheck = (value) => {
+  i: {
+    className: `bi ${value ? 'bi-check-square' : 'bi-square'}`.at;
+  }
+};
