25bee3d49c44aa7687649ee56a3cb3b415db4c6f
Satyam <satyam@satyam.com.ar>
2025-09-08 15:24:44 +0200

    Added `path-to-regexp` to handle Express style routes.  Used it in `Routes`

--- juris-client/src/components/Routes.js
+++ juris-client/src/components/Routes.js
@@ -1,27 +1,39 @@
 import juris from '@src/jurisInstance.js';
+import { match } from 'path-to-regexp';
+
 import '@pages/Home.js';
 import '@pages/Login.js';
 import '@pages/vendedor/ListVendedores.jsx';
 import '@pages/ventas/ListVentas.js';
 
-juris.registerComponent('Routes', (props, { getState }) => ({
-  render: () => ({
-    div: {
-      children: () => {
-        const path = getState('url.path');
-        switch (path) {
-          case '/':
-            return { Home: {} };
-          case '/login':
-            return { Login: {} };
-          case '/vendedores':
-            return { ListVendedores: {} };
-          case '/ventas':
-            return { ListVentas: {} };
-          default:
-            return { NotFoundPage: {} };
-        }
+const routes = {
+  '/': 'Home',
+  '/login': 'Login',
+  '/vendedores': 'ListVendedores',
+  '/ventas': 'ListVentas',
+};
+
+juris.registerComponent('Routes', (props, { getState }) => {
+  const _routes = Object.entries(routes).reduce(
+    (r, [route, path]) => [...r, match(route), path],
+    []
+  );
+
+  return {
+    render: () => ({
+      div: {
+        children: () => {
+          const path = getState('url.path');
+          const rLen = _routes.length;
+          for (let i = 0; i < rLen; i += 2) {
+            const match = _routes[i](path);
+            if (match) {
+              return { [_routes[i + 1]]: match.params };
+            }
+          }
+          return { NotFoundPage: {} };
+        },
       },
-    },
-  }),
-}));
+    }),
+  };
+});
