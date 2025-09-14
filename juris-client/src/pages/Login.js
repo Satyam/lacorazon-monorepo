ebcd922854dbc6de7730b4a1b6cd94338f8e9198
Satyam <satyam@satyam.com.ar>
2025-09-14 18:30:36 +0200

    Changed `Login.js` to use `Form` and it works.

--- juris-client/src/pages/Login.js
+++ juris-client/src/pages/Login.js
@@ -3,31 +3,23 @@ import juris from '@src/jurisInstance.js';
 import '@headless/Navigation.js';
 import '@headless/User.js';
 
-import { textField } from '@src/utils.js';
-
+import '@components/Forms.js';
 juris.registerComponent(
   'Login',
   (props, { setState, getState, newState, User, Navigation }) => {
-    const [getEmail, setEmail] = newState('email', 'pepe@correo.com');
-    const [getPassword, setPassword] = newState('password', 'pepecito');
-    const [getSubmitting, setSubmitting] = newState('submitting', false);
     const [get401, set401] = newState('unauthorized', false);
     return {
       render: () => {
         setState('title', 'Login');
         return {
-          form: {
-            novalidate: true,
-            onsubmit: (ev) => {
-              ev.preventDefault();
-              setSubmitting(true);
-              User.login(getEmail(), getPassword())
+          Form: {
+            name: 'login',
+            onsubmit: ({ email, password }) => {
+              User.login(email, password)
                 .then(() => {
-                  setSubmitting(false);
                   Navigation.back();
                 })
                 .catch(({ error, data }) => {
-                  setSubmitting(false);
                   if (error === 401) {
                     set401(true);
                     console.info('unauthorized');
@@ -50,33 +42,37 @@ juris.registerComponent(
                       },
                     }
                   : null,
-              textField('email', 'Email', () => getEmail(), {
-                required: true,
-                invalid: get401(),
-                oninput: (ev) => setEmail(ev.target.value),
-                errorText: 'Debe indicar la dirección de correo registrada',
-                placeholder: 'Email',
-              }),
-              textField('password', 'Contraseña', () => getPassword(), {
-                type: 'password',
-                placeholder: 'Contraseña',
-                required: true,
-                oninput: (ev) => setPassword(ev.target.value),
-                invalid: get401(),
-                errorText: 'Debe indicar una contraseña',
-              }),
               {
-                button: {
-                  type: 'submit',
-                  className: 'btn btn-primary',
-                  disabled: () =>
-                    !(getEmail() || getPassword()) || getSubmitting(),
-                  text: 'Acceder',
+                TextField: {
+                  name: 'email',
+                  label: 'Email',
+                  value: 'pepe@correo.com',
+                  required: true,
+                  invalid: () => get401(),
+                  errorText: 'Debe indicar la dirección de correo registrada',
+                  placeholder: 'Email',
                 },
-              }, // button
+              },
+              {
+                TextField: {
+                  name: 'password',
+                  label: 'Contraseña',
+                  type: 'password',
+                  value: 'pepecito',
+                  placeholder: 'Contraseña',
+                  required: true,
+                  invalid: () => get401(),
+                  errorText: 'Debe indicar una contraseña',
+                },
+              },
+              {
+                SubmitButton: {
+                  label: 'Acceder',
+                },
+              },
             ],
           },
-        }; // form
+        };
       },
     };
   }
