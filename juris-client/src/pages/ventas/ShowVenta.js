9c02a81615ec0746b4affbecaa24c5dabb7caea1
Satyam <satyam@satyam.com.ar>
2025-09-15 17:00:30 +0200

    Switched `SHowVenta` to use my `Form` control

--- juris-client/src/pages/ventas/ShowVenta.js
+++ juris-client/src/pages/ventas/ShowVenta.js
@@ -1,12 +1,7 @@
 import juris from '@src/jurisInstance.js';
 import '@headless/DataApi.js';
-
-import {
-  formatDate,
-  formatCurrency,
-  textField,
-  checkboxField,
-} from '@src/utils.js';
+import '@components/Forms.js';
+import { formatDate, formatCurrency } from '@src/utils.js';
 
 juris.registerComponent('ShowVenta', async ({ id }, { setState, DataApi }) => {
   const venta = await DataApi.getVenta(id);
@@ -14,31 +9,59 @@ juris.registerComponent('ShowVenta', async ({ id }, { setState, DataApi }) => {
     render: () => {
       setState('title', 'Venta');
       return {
-        form: {
+        Form: {
+          name: 'ShowVenta',
           children: [
-            textField('fecha', 'Fecha', () => formatDate(venta.fecha)),
-            textField('concepto', 'Concepto', () => venta.concepto || '', {
-              readonly: true,
-            }),
-            textField('cantidad', 'Cantidad', () => venta.cantidad || 0, {
-              readonly: true,
-            }),
-            textField(
-              'precioUnitario',
-              'Precio Unitario',
-              () => formatCurrency(venta.precioUnitario || 0),
-              { readonly: true }
-            ),
-            checkboxField('iva', 'IVA', !!venta.iva, { readonly: true }),
-            textField(
-              'precioTotal',
-              'Precio Total',
-              () =>
-                formatCurrency(
+            {
+              TextField: {
+                name: 'fecha',
+                label: 'Fecha',
+                value: formatDate(venta.fecha),
+                readonly: true,
+              },
+            },
+            {
+              TextField: {
+                name: 'concepto',
+                label: 'Concepto',
+                value: venta.concepto || '',
+                readonly: true,
+              },
+            },
+            {
+              TextField: {
+                name: 'cantidad',
+                label: 'Cantidad',
+                value: venta.cantidad || 0,
+                readonly: true,
+              },
+            },
+            {
+              TextField: {
+                name: 'precioUnitario',
+                label: 'Precio Unitario',
+                value: formatCurrency(venta.precioUnitario || 0),
+                readonly: true,
+              },
+            },
+            {
+              CheckboxField: {
+                name: 'iva',
+                label: 'IVA',
+                value: !!venta.iva,
+                readonly: true,
+              },
+            },
+            {
+              TextField: {
+                name: 'precioTotal',
+                label: 'Precio Total',
+                value: formatCurrency(
                   (venta?.cantidad || 0) * (venta?.precioUnitario || 0)
                 ),
-              { readonly: true }
-            ),
+                readonly: true,
+              },
+            },
           ],
         },
       };
