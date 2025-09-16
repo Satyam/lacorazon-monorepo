5962cb90adebc37f33330897813ca54f8793aea4
Satyam <satyam@satyam.com.ar>
2025-09-16 14:37:55 +0200

    Added option to include extra params in routes. Added routes for `EditVenta`.

--- juris-client/src/components/Routes.js
+++ juris-client/src/components/Routes.js
@@ -6,12 +6,15 @@ import '@pages/Login.js';
 import '@pages/vendedor/ListVendedores.jsx';
 import '@pages/ventas/ListVentas.js';
 import '@pages/ventas/ShowVenta.js';
+import '@pages/ventas/EditVenta.js';
 
 const routes = {
   '/': 'Home',
   '/login': 'Login',
   '/vendedores': 'ListVendedores',
   '/ventas': 'ListVentas',
+  '/venta/edit/:id': 'EditVenta',
+  '/venta/new': ['EditVenta', { isNew: true }],
   '/venta/:id': 'ShowVenta',
 };
 
@@ -30,14 +33,17 @@ juris.registerComponent('Routes', (props, { getState }) => {
           for (let i = 0; i < rLen; i += 2) {
             const match = _routes[i](path);
             if (match) {
+              let dest = _routes[i + 1];
+              let params = match.params;
+              if (Array.isArray(dest)) {
+                params = { ...params, ...dest[1] };
+                dest = dest[0];
+              }
               return {
                 // The `Object.setPrototyepOf` is needed because `match.params` is a
                 // "null-prototype object".
                 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object#null-prototype_objects
-                [_routes[i + 1]]: Object.setPrototypeOf(
-                  match.params,
-                  Object.prototype
-                ),
+                [dest]: Object.setPrototypeOf(params, Object.prototype),
               };
             }
           }
